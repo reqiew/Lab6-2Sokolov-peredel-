64cecffe7b3beb35a090b33471f82c7274732073
dobry <dobry@DESKTOP-FBD3PAQ>
2023-12-26 14:19:41 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static double[] abs(double[,] matrix)
+        {
+            double[] maxElements = new double[6];
+            double[] mas = new double[6];
+            for (int i = 0; i < 6; i++)
+            {
+                double max = matrix[i, 0];
+                for (int j = 1; j < 4; j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                }
+                maxElements[i] = max;
+            }
+
+            return maxElements;
+
+        }
+
+
+
+    }
+}
---
+++ Lab6-2Sokolov(peredel)/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary1;
+try
+{
+    double[,] matrix = new double[6, 4];
+
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            Console.Write($"Введите элемент [{i}, {j}]: ");
+            matrix[i, j] = double.Parse(Console.ReadLine()!);
+        }
+    }
+
+    double[] maxElements = ClassLibrary1.Class1.abs(matrix);
+    Console.WriteLine("максимальные элементы строк матрицы");
+    for (int i = 0; i < 6; i++)
+    {
+        Console.Write(maxElements[i] + " ");
+    }
+}
+catch (Exception ex)
+{ Console.WriteLine(ex.Message); }
